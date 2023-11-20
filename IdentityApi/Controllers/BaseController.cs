af09a379dba63f42c2666bafa6b9c3a94a947dc3
Egor Dubrovskiy <holcan@mail.ru>
2023-11-20 21:29:25 +0300

    Install necessary packages

---
+++ IdentityApi/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace IdentityApi.Controllers;
+
+public class BaseController : Controller
+{
+    public static string GetErrorMessage(ModelStateDictionary modelState)
+    {
+        return string.Join(" ", modelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)));
+    }
+}
